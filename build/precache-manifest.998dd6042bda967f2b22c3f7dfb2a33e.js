8e688b345384a00d5936bdca529dbd7b8b959d32
long nguyen <long.ngyen91@gmail.com>
2020-05-17 16:43:10 +0700

    Build initial

---
+++ build/precache-manifest.998dd6042bda967f2b22c3f7dfb2a33e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055907e07a0d915d0a3cfd582fb515fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc5e0bfed98d13d8ebd1",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c02efecb106f753f9599",
+    "url": "/static/js/2.94f8ea0b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.94f8ea0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc5e0bfed98d13d8ebd1",
+    "url": "/static/js/main.bffcc6d3.chunk.js"
+  },
+  {
+    "revision": "f88c7e80ccf8e97a2429",
+    "url": "/static/js/runtime-main.8b1a6544.js"
+  }
+]);
