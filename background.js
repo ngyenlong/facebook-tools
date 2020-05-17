ec12d28f360b7994e5ee0c1d3c38263085761eec
long nguyen <long.ngyen91@gmail.com>
2020-05-17 17:17:47 +0700

    Updates

---
+++ background.js
@@ -0,0 +1,45 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log('The color is green.');
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: {hostContains: 'facebook.com'},
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
+
+function focusOrCreateTab(url) {
+  chrome.windows.getAll({"populate":true}, function(windows) {
+    var existing_tab = null;
+    for (var i in windows) {
+      var tabs = windows[i].tabs;
+      for (var j in tabs) {
+        var tab = tabs[j];
+        if (tab.url == url) {
+          existing_tab = tab;
+          break;
+        }
+      }
+    }
+    if (existing_tab) {
+      chrome.tabs.update(existing_tab.id, {"selected":true});
+    } else {
+      chrome.tabs.create({"url":url, "selected":true});
+    }
+  });
+}
+chrome.browserAction.onClicked.addListener(function(tab) {
+  var manager_url = chrome.extension.getURL("index.html");
+  focusOrCreateTab(manager_url);
+});
+
---
+++ precache-manifest.c0ba18ef69afc7e1ea94850d6fe65e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "668e38ef3b91a155917128bc20649d1c",
+    "url": "/ngyenlong/facebook-tools/index.html"
+  },
+  {
+    "revision": "fafed95a8caf797f12f9",
+    "url": "/ngyenlong/facebook-tools/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2fe1feab6d76a65b559f",
+    "url": "/ngyenlong/facebook-tools/static/js/2.43ae9f61.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ngyenlong/facebook-tools/static/js/2.43ae9f61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fafed95a8caf797f12f9",
+    "url": "/ngyenlong/facebook-tools/static/js/main.bbd4c70b.chunk.js"
+  },
+  {
+    "revision": "de055f3daab4116c5986",
+    "url": "/ngyenlong/facebook-tools/static/js/runtime-main.ec8e532f.js"
+  }
+]);
